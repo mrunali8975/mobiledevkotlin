a01a2bf6e76c1ff27df2473560974c1563c6ab2c
Mrunali Ambhore <mrunaliambhore1999@gmail.com>
2022-02-13 00:42:08 +0800

    first commit

---
+++ App.js
@@ -0,0 +1,73 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+
+import { View, Text, SafeAreaView, StatusBar,StyleSheet,ScrollView } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <SafeAreaView style={page.container}>
+      <StatusBar barStyle={'light-content'}></StatusBar>
+      <ScrollView >
+        <Text style={page.textcolor}> Hello </Text>
+        <Text style={page.textcolor}> Hello </Text>
+        <Text style={page.textcolor}> Hello </Text>
+        <Text style={page.textcolor}> Hello </Text>
+        <Text style={page.textcolor}> Hello </Text>
+        <Text style={page.textcolor}> Hello </Text>
+        <Text style={page.textcolor}> Hello </Text>
+        <Text style={page.textcolor}> Hello </Text>
+        <Text style={page.textcolor}> Hello </Text>
+        <Text style={page.textcolor}> Hello </Text>
+        <Text style={page.textcolor}> Hello </Text>
+        <Text style={page.textcolor}> Hello </Text>
+        <Text style={page.textcolor}> Hello </Text>
+        <Text style={page.textcolor}> Hello </Text>
+        <Text style={page.textcolor}> Hello </Text>
+        <Text style={page.textcolor}> Hello </Text>
+        <Text style={page.textcolor}> Hello </Text>
+        <Text style={page.textcolor}> Hello </Text>
+        <Text style={page.textcolor}> Hello </Text>
+        <Text style={page.textcolor}> Hello </Text>
+        <Text style={page.textcolor}> Hello </Text>
+        <Text style={page.textcolor}> Hello </Text>
+        <Text style={page.textcolor}> Hello </Text>
+        <Text style={page.textcolor}> Hello </Text>
+        <Text style={page.textcolor}> Hello </Text>
+        <Text style={page.textcolor}> Hello </Text>
+        <Text style={page.textcolor}> Hello </Text>
+        <Text style={page.textcolor}> Hello </Text>
+        <Text style={page.textcolor}> Hello </Text>
+        <Text style={page.textcolor}> Hello </Text>
+        <Text style={page.textcolor}> Hello </Text>
+        <Text style={page.textcolor}> Hello </Text>
+        <Text style={page.textcolor}> Hello </Text>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+const page = StyleSheet.create({
+container:
+{
+  padding:20,
+ backgroundColor:"#999",
+ flex: 1
+
+  
+},
+textcolor:
+{ color:'#000',
+  textAlign:'center',
+
+}
+});     
+export default App;    
+
+
+
