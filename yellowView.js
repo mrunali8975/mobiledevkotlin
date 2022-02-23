553830b0cd9af776e47a1382f0f34716150b31bb
sonalshendre <sonalshendre07@gmail.com>
2022-02-23 21:01:17 +0800

    sonal commit

--- yellowView.js
+++ yellowView.js
@@ -1,26 +1,27 @@
 import { View, Text ,StyleSheet, TouchableOpacity, Image} from 'react-native'
 import React from 'react'
-import Gagte from './gagatesimage.jpeg'
+
 import lap from './laptop.jpeg'
 import Headp from './headp.png'
-import Ipad from '/Users/user214291/flipkart/ipadd.jpeg'
-
+import wg from './wg.png'
+import Ipad from '/Users/user214291/flipkart/ipad.jpeg'
+import Camera from './cameraa.jpeg'
 
 export default function yellowView() {
   return (
     <View style={styles.mainContainer}>
       <View style={styles.fristInnerCon}>
           <View
-          style={{width:'65%', height:150,justifyContent:'flex-start',padding:10}}>
-              <Text style={{fontSize:23 , paddingTop:20, fontWeight:"bold", color:'black'}}>Entertaintment Zone</Text>
+          style={{width:'60%', height:150,justifyContent:'flex-start',}}>
+              <Text style={{fontSize:22 , paddingTop:30, fontWeight:"bold", color:'black'}}>Entertaintment Zone</Text>
               <Text style={{color:'black',fontSize:13}}
               > For Some Cozy Envornment{"!"}</Text>
-              <TouchableOpacity style={{width:70, borderRadius:5 ,backgroundColor:"white",padding:10, marginTop:10}} >
+              <TouchableOpacity style={{width:65, borderRadius:5 ,backgroundColor:"white",padding:5, marginTop:10}} >
                   <Text style={{color:'black'}}>View all</Text>
               </TouchableOpacity>
           </View>
-          <View style={{width:'35%'}}>
-              <Image source={Gagte} style={{height:150,width:'100%',padding:10}}/>
+          <View style={{width:'40%'}}>
+              <Image source={wg} style={{height:150,width:'100%',}}/>
           </View>
       </View>
       <View style={styles.secInnerCon}>
@@ -28,33 +29,33 @@ export default function yellowView() {
          <Image source={lap} style={{width:50,height:50, }} />
          <View style={{paddingLeft:5}}>
          <Text style={{color:'black'}}>Chromebooks</Text>
-             <Text style={{color:'black',fontSize:12}}>browsing {"&"} more</Text>
-             <Text style={{fontWeight:'bold' ,color:'black',fontSize:17}}>shop Now</Text>
+             <Text style={{color:'black',fontSize:11}}>browsing {"&"} more</Text>
+             <Text style={{fontWeight:'bold' ,color:'black',fontSize:17,paddingTop:5}}>shop Now</Text>
          </View>
           </View>
           <View style={[styles.iconcon_secInnerCon,{marginBottom:5}]}>
          <Image source={Headp} style={{width:35,height:50, }} />
          <View style={{paddingLeft:10}}>
          <Text style={{color:'black'}}>Headphones{"&"}more</Text>
-             <Text style={{color:'black',fontSize:12}}>JBL boAt {"&"} more</Text>
+             <Text style={{color:'black',fontSize:11}}>JBL boAt {"&"} more</Text>
              
-             <Text style={{fontWeight:'bold' ,color:'black',fontSize:17}}>Up to 70{"%"} Off</Text>
+             <Text style={{fontWeight:'bold' ,color:'black',fontSize:17,paddingTop:5}}>Up to 70{"%"} Off</Text>
          </View>
           </View>
           <View style={[styles.iconcon_secInnerCon,{marginRight:5}]}>
-         <Image source={Ipad} style={{width:50,height:50}} />
+         <Image source={Ipad} style={{width:50,height:60}} />
          <View style={{paddingLeft:5}}>
-             <Text style={{color:'black'}}>Chromebooks</Text>
-             <Text style={{color:'black',fontSize:12}}>browsing {"&"} more</Text>
-             <Text style={{fontWeight:'bold' ,color:'black',fontSize:17}}>shop Now</Text>
+             <Text style={{color:'black'}}>Ipad Air 4th Gen</Text>
+             <Text style={{color:'black',fontSize:11}}>Bestseller</Text>
+             <Text style={{fontWeight:'bold' ,color:'black',fontSize:17,paddingTop:5}}>Just {"₹"}54,900</Text>
          </View>
           </View>
           <View style={[styles.iconcon_secInnerCon]}>
-         <Image source={lap} style={{width:50,height:50, }} />
-         <View style={{paddingLeft:10}}>
-             <Text style={{color:'black'}}>Chromebooks</Text>
-             <Text style={{color:'black',fontSize:12}}>browsing {"&"} more</Text>
-             <Text style={{fontWeight:'bold' ,color:'black',fontSize:17}}>shop Now</Text>
+         <Image source={Camera} style={{width:50,height:60, }} />
+         <View style={{paddingLeft:5}}>
+             <Text style={{color:'black'}}>Vlogging Cameras</Text>
+             <Text style={{color:'black',fontSize:11}}>Vlog like never Before</Text>
+             <Text style={{fontWeight:'bold' ,color:'black',fontSize:17,paddingTop:5}}>Up to 30{"%"} Off</Text>
          </View>
           </View>
       </View>
@@ -63,26 +64,28 @@ export default function yellowView() {
 }const styles = StyleSheet.create ({
     mainContainer:{
       backgroundColor:'#ebcd34',
-      flex:1,
+      borderRadius:10,
+      margin:10
+    
     },
     fristInnerCon:{
-      flex:1,
+     
       flexDirection: 'row'
       ,justifyContent:'flex-start',
       flexWrap:'nowrap',
-      padding: 10,
+     padding:10,
       alignContent:'center',
     
     },
      
     secInnerCon:{
-        flex:1,
+       
         flexDirection: 'row'
         ,justifyContent:'center',
         flexWrap:'wrap',
-        padding: 10,
+         marginVertical:10,
         alignContent:'center',
-        
+       
       },
 
       iconcon_secInnerCon:{
@@ -90,8 +93,8 @@ export default function yellowView() {
         flexDirection:'row',
         borderRadius:10,
         backgroundColor:'#ffe6e6',
-        padding:10,
-       
+        padding:5
+        
         
       }
 
